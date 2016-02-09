18ffe72a31db37e97590563f693d85dcad44dbcb
James Scott <jss42@talk21.com>
2016-02-09 13:01:46 -0500

    Added comments

--- rexray2.sh
+++ rexray2.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+#Simple script to automate the docker environment with Rexray on Windows from the docker toolset
+# Created by James Scott based on work from blog.emc.com
+# The virtualbox server should be running prior to launching this see "rexray.bat"
+# Work in progrss :)
+
 printf "Creating Docker Machine.... "
 echo
 CHECKDM="$(docker-machine status testing)"
+
+#Don't create the machine if it's already running
+#I've had rexray issues re-starting the machine so just re-create if it's not running
 if [[ $CHECKDM == *"Running"* ]]
 then
   echo "Docker Machine already running"
@@ -12,6 +20,8 @@ else
   # Re-create
   docker-machine create --driver=virtualbox testing
   printf "Downloading and Installing Rexray...."
+  #Download and install rexray
+  #Need to expand this to save the install locally for offline use
   docker-machine ssh testing "curl -sSL https://dl.bintray.com/emccode/rexray/install | sh -"
   printf "Setting Rexray Configuration to VirtualBox"
   docker-machine ssh testing "sudo tee -a /etc/rexray/config.yml << EOF
@@ -31,6 +41,9 @@ else
   docker-machine ssh testing "sudo rexray start"
 fi
 printf "Setting environment to Docker Machine"
+
+#Options to test the rexray environment
+
 echo
 eval $(docker-machine env testing)
 read -p "Do you want to create a test volume? " -n 1 -r
