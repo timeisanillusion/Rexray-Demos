198888f678115267239617ae46d7f52bbd78a3be
James Scott <jss42@talk21.com>
2016-02-09 21:14:44 -0500

    Added comments

--- rexray.bat
+++ rexray.bat
@@ -1,5 +1,4 @@
 rem Windows script to start the virtualbox web service ready for rexray
-
 cd "c:\Program Files\Oracle\VirtualBox\
 VBoxManage setproperty websrvauthlibrary null
 start vboxwebsrv -H 0.0.0.0 -v
